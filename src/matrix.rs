492b4eab7e0e7a0330aeb4ef66b7bebb98a3e6a5
j.v.o.koskinen@gmail.com <j.v.o.koskinen@gmail.com>
2024-07-31 09:24:04 +0300

    rewrite BitMatrix8x8::mul with safe code

--- src/matrix.rs
+++ src/matrix.rs
@@ -172,20 +172,20 @@ impl Mul for BitMatrix8x8 {
         const ROW: u64 = 0x00000000000000FF;
         const COL: u64 = 0x0101010101010101;
 
-        let mut rows: [u8; 8] = [0; 8];
-        let this = self.0.as_ptr() as *const u64;
-        let rhs = rhs.0.as_ptr() as *const u64;
-        let mul = rows.as_mut_ptr() as *mut u64;
+        let this = u64::from_ne_bytes(self.0);
+        let rhs = u64::from_ne_bytes(rhs.0);
+        let mut result = 0;
 
-        unsafe {
-            for i in 0..8 {
-                let p = COL & (*this >> i);
-                let r = ROW & (*rhs >> (i * 8));
-                *mul |= p * r;
-            }
-        }
+        result |= (COL & this) * (ROW & rhs);
+        result |= (COL & (this >> 1)) * (ROW & (rhs >> 8));
+        result |= (COL & (this >> 2)) * (ROW & (rhs >> 16));
+        result |= (COL & (this >> 3)) * (ROW & (rhs >> 24));
+        result |= (COL & (this >> 4)) * (ROW & (rhs >> 32));
+        result |= (COL & (this >> 5)) * (ROW & (rhs >> 40));
+        result |= (COL & (this >> 6)) * (ROW & (rhs >> 48));
+        result |= (COL & (this >> 7)) * (ROW & (rhs >> 56));
 
-        Self(rows)
+        Self(result.to_ne_bytes())
     }
 }
 
@@ -209,7 +209,7 @@ impl Mul<&BitMatrix8x8> for &BitMatrix8x8 {
 
 impl Mul<BitMatrix8x8> for &BitMatrix8x8 {
     type Output = <BitMatrix8x8 as Mul>::Output;
-    
+
     /// Multiplies the matrix by `rhs`. See [BitMatrix8x8::mul] for an example.
     fn mul(self, rhs: BitMatrix8x8) -> BitMatrix8x8 {
         *self * rhs
@@ -223,67 +223,42 @@ mod tests {
     #[test]
     fn mul_8x8() {
         let matrix = BitMatrix8x8::new([
-            0b1000_0000,
-            0b1100_0000,
-            0b1110_0000,
-            0b1111_0000,
-            0b1111_1000,
-            0b1111_1100,
-            0b1111_1110,
-            0b1111_1111,
+            0b_1_1_1_1_1_1_1_1,
+            0b_0_1_1_1_1_1_1_1,
+            0b_0_0_1_1_1_1_1_1,
+            0b_0_0_0_1_1_1_1_1,
+            0b_0_0_0_0_1_1_1_1,
+            0b_0_0_0_0_0_1_1_1,
+            0b_0_0_0_0_0_0_1_1,
+            0b_0_0_0_0_0_0_0_1,
         ]);
-        let other = BitMatrix8x8::new([
-            0b0000_0001,
-            0b0000_0010,
-            0b0000_0100,
-            0b0000_1000,
-            0b0001_0000,
-            0b0010_0000,
-            0b0100_0000,
-            0b1000_0000,
-        ]);
-        println!("matrix = \n{}", matrix);
-        println!("other = \n{}", other);
-        println!("identity = \n{}", BitMatrix8x8::IDENTITY);
-        println!("matrix * other = \n{}", matrix * other);
-        println!("matrix * identity = \n{}", matrix * BitMatrix8x8::IDENTITY);
-        // let expect = BitMatrix8x8([
-        //     0b1111_1111,
-        //     0b1111_1110,
-        //     0b1111_1100,
-        //     0b1111_1000,
-        //     0b1111_0000,
-        //     0b1110_0000,
-        //     0b1100_0000,
-        //     0b1000_0000,
-        // ]);
-        // assert_eq!(result, expect);
+        assert_eq!(matrix * BitMatrix8x8::IDENTITY, matrix);
     }
 
     #[test]
     fn transpose_8x8() {
         let mut matrix = BitMatrix8x8::new([
-            0b1000_0000,
-            0b1100_0000,
-            0b1110_0000,
-            0b1111_0000,
-            0b1111_1000,
-            0b1111_1100,
-            0b1111_1110,
-            0b1111_1111,
+            0b_1_0_0_0_0_0_0_0,
+            0b_1_1_0_0_0_0_0_0,
+            0b_1_1_1_0_0_0_0_0,
+            0b_1_1_1_1_0_0_0_0,
+            0b_1_1_1_1_1_0_0_0,
+            0b_1_1_1_1_1_1_0_0,
+            0b_1_1_1_1_1_1_1_0,
+            0b_1_1_1_1_1_1_1_1,
         ]);
         matrix.transpose();
         assert_eq!(
             matrix,
             BitMatrix8x8([
-                0b1111_1111,
-                0b0111_1111,
-                0b0011_1111,
-                0b0001_1111,
-                0b0000_1111,
-                0b0000_0111,
-                0b0000_0011,
-                0b0000_0001,
+                0b_1_1_1_1_1_1_1_1,
+                0b_0_1_1_1_1_1_1_1,
+                0b_0_0_1_1_1_1_1_1,
+                0b_0_0_0_1_1_1_1_1,
+                0b_0_0_0_0_1_1_1_1,
+                0b_0_0_0_0_0_1_1_1,
+                0b_0_0_0_0_0_0_1_1,
+                0b_0_0_0_0_0_0_0_1,
             ])
         );
     }
