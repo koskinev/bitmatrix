f1d5c165826c5e3d8d8890ed99895e775b2fd0dc
j.v.o.koskinen@gmail.com <j.v.o.koskinen@gmail.com>
2024-07-31 13:05:21 +0300

    represent bit matrices as a trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 mod unsigned;
 mod matrix;
 
-pub use matrix::BitMatrix8x8;
+pub use matrix::BitMatrix;
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,13 +1,107 @@
-use core::fmt::{Debug, Display};
-use core::ops::Mul;
 
-#[derive(Clone, Copy, Default, PartialEq, Eq)]
-pub struct BitMatrix8x8([u8; 8]);
+/// A trait for bit matrices.
+pub trait BitMatrix {
+    /// The number of rows and columns in the matrix.
+    const SIZE: (usize, usize);
 
-impl BitMatrix8x8 {
-    /// The 8x8 identity matrix. This is the matrix with all bits set to 0, except for the diagonal
+    /// The identity matrix. This is the matrix with all bits set to 0, except for the diagonal
     /// from the top-left to the bottom-right, which is set to 1.
-    pub const IDENTITY: Self = Self([
+    const IDENTITY: Self;
+
+    /// The zero matrix. This is the matrix with all bits set to 0.
+    const ZERO: Self;
+
+    /// The type used to represent a row in the matrix.
+    type Row;
+
+    /// Returns the number of ´1´-bits in the matrix.
+    fn count_ones(&self) -> u32;
+
+    /// Returns the number of ´0´-bits in the matrix.
+    fn count_zeros(&self) -> u32;
+
+    /// Multiplies the matrix by `rhs`.
+    ///
+    /// ```
+    /// use bitmatrix::BitMatrix;
+    /// 
+    /// type BitMatrix8x8 = [u8; 8];
+    /// let matrix = [
+    ///     0b_1_1_1_1_1_1_1_1,
+    ///     0b_0_1_1_1_1_1_1_1,
+    ///     0b_0_0_1_1_1_1_1_1,
+    ///     0b_0_0_0_1_1_1_1_1,
+    ///     0b_0_0_0_0_1_1_1_1,
+    ///     0b_0_0_0_0_0_1_1_1,
+    ///     0b_0_0_0_0_0_0_1_1,
+    ///     0b_0_0_0_0_0_0_0_1,
+    /// ];
+    /// assert_eq!(matrix * BitMatrix8x8::IDENTITY, matrix);
+    /// ```
+    fn matmul(self, rhs: &Self) -> Self;
+
+    /// Reverses the order of the rows.
+    fn reverse_rows(&mut self);
+
+    /// Reverses the order of the columns.
+    fn reverse_columns(&mut self);
+
+    /// Sorts the bits of each row so that all ´1´-bits in a row are moved to the most significant
+    /// positions.
+    fn sort_row_bits(&mut self);
+
+    /// Sorts the bits of each column so that all ´1´-bits in a column are moved to the last rows of
+    /// the matrix.
+    fn sort_column_bits(&mut self);
+
+    /// Transposes the matrix in-place.
+    ///
+    /// ```
+    /// use bitmatrix::BitMatrix;
+    /// 
+    /// let mut matrix = [
+    ///     0b_1_0_0_0_0_0_0_0,
+    ///     0b_1_1_0_0_0_0_0_0,
+    ///     0b_1_1_1_0_0_0_0_0,
+    ///     0b_1_1_1_1_0_0_0_0,
+    ///     0b_1_1_1_1_1_0_0_0,
+    ///     0b_1_1_1_1_1_1_0_0,
+    ///     0b_1_1_1_1_1_1_1_0,
+    ///     0b_1_1_1_1_1_1_1_1,
+    /// ];
+    ///
+    /// matrix.transpose();
+    /// assert_eq!(
+    ///     matrix,
+    ///     [
+    ///         0b_1_1_1_1_1_1_1_1,
+    ///         0b_0_1_1_1_1_1_1_1,
+    ///         0b_0_0_1_1_1_1_1_1,
+    ///         0b_0_0_0_1_1_1_1_1,
+    ///         0b_0_0_0_0_1_1_1_1,
+    ///         0b_0_0_0_0_0_1_1_1,
+    ///         0b_0_0_0_0_0_0_1_1,
+    ///         0b_0_0_0_0_0_0_0_1
+    ///     ])
+    /// );
+    /// ```
+    fn transpose(&mut self);
+
+    /// Returns a transposed copy of the matrix.
+    fn transposed(&self) -> Self
+    where
+        Self: Copy,
+    {
+        let mut mat = *self;
+        mat.transpose();
+        mat
+    }
+}
+
+impl BitMatrix for [u8; 8] {
+    type Row = u8;
+
+    const IDENTITY: Self = [
         0b_0_0_0_0_0_0_0_1,
         0b_0_0_0_0_0_0_1_0,
         0b_0_0_0_0_0_1_0_0,
@@ -16,67 +110,76 @@ impl BitMatrix8x8 {
         0b_0_0_1_0_0_0_0_0,
         0b_0_1_0_0_0_0_0_0,
         0b_1_0_0_0_0_0_0_0,
-    ]);
-    /// The 8x8 zero matrix. This is the matrix with all bits set to 0.
-    pub const ZERO: Self = Self([0; 8]);
+    ];
+    const SIZE: (usize, usize) = (8, 8);
+    const ZERO: Self = [0; 8];
 
-    /// Creates a new bit matrix with the given rows.
-    ///
-    /// ```
-    /// # use bitmatrix::BitMatrix8x8;
-    /// let matrix = BitMatrix8x8::new([
-    ///     0b1000_0000,
-    ///     0b0100_0000,
-    ///     0b0010_0000,
-    ///     0b0001_0000,
-    ///     0b0000_1000,
-    ///     0b0000_0100,
-    ///     0b0000_0010,
-    ///     0b0000_0001,
-    /// ]);
-    ///
-    /// assert_eq!(matrix, BitMatrix8x8::IDENTITY);
-    /// ```
-    pub fn new(rows: [u8; 8]) -> Self {
-        Self(rows)
+    fn count_ones(&self) -> u32 {
+        self.iter().fold(0, |acc, row| acc + row.count_ones())
     }
 
-    /// Returns the rows of the matrix as a slice of bytes.
-    pub fn rows(&self) -> &[u8] {
-        &self.0
+    fn count_zeros(&self) -> u32 {
+        self.iter().fold(0, |acc, row| acc + row.count_zeros())
     }
 
-    /// Returns the rows of the matrix as a mutable slice of bytes.
-    pub fn rows_mut(&mut self) -> &mut [u8] {
-        &mut self.0
+    fn matmul(self, rhs: &Self) -> Self {
+        const ROW: u64 = 0x00000000000000FF;
+        const COL: u64 = 0x0101010101010101;
+
+        let this = u64::from_ne_bytes(self);
+        let rhs = u64::from_ne_bytes(*rhs);
+        let mut result = 0;
+
+        result |= (COL & this) * (ROW & rhs);
+        result |= (COL & (this >> 1)) * (ROW & (rhs >> 8));
+        result |= (COL & (this >> 2)) * (ROW & (rhs >> 16));
+        result |= (COL & (this >> 3)) * (ROW & (rhs >> 24));
+        result |= (COL & (this >> 4)) * (ROW & (rhs >> 32));
+        result |= (COL & (this >> 5)) * (ROW & (rhs >> 40));
+        result |= (COL & (this >> 6)) * (ROW & (rhs >> 48));
+        result |= (COL & (this >> 7)) * (ROW & (rhs >> 56));
+
+        result.to_ne_bytes()
     }
 
-    /// Transposes the matrix in-place.
-    ///
-    /// ```
-    /// # use bitmatrix::BitMatrix8x8;
-    /// let mut row = 0_u8;
-    /// let mut rows = core::array::from_fn(move |_| {
-    ///     row = (row << 1) + 1;
-    ///     row.reverse_bits()
-    /// });
-    /// let mut matrix = BitMatrix8x8::new(rows);
-    /// matrix.transpose();
-    /// assert_eq!(
-    ///     matrix.rows(),
-    ///     &[
-    ///         0b1111_1111,
-    ///         0b0111_1111,
-    ///         0b0011_1111,
-    ///         0b0001_1111,
-    ///         0b0000_1111,
-    ///         0b0000_0111,
-    ///         0b0000_0011,
-    ///         0b0000_0001,
-    ///     ]
-    /// );
-    /// ```
-    pub fn transpose(&mut self) {
+    fn reverse_rows(&mut self) {
+        self.reverse();
+    }
+
+    fn reverse_columns(&mut self) {
+        for row in self.iter_mut() {
+            *row = row.reverse_bits();
+        }
+    }
+
+    fn sort_row_bits(&mut self) {
+        for row in self.iter_mut() {
+            let ones = row.count_ones();
+            let (x, overflow) = row.overflowing_shl(ones);
+            *row = (x - 1) * (!overflow as u8);
+        }
+    }
+
+    fn sort_column_bits(&mut self) {
+        let mut mask = 0;
+        let mut unsorted = 0;
+        for row in &*self {
+            mask |= *row;
+            unsorted |= *row ^ mask;
+        }
+        while unsorted > 0 {
+            mask = 1 << unsorted.trailing_zeros();
+            if let Some(l) = self.iter().position(|row| row & mask != 0) {
+                if let Some(x) = self[l..].iter().rposition(|row| row & mask == 0) {
+                    let r = x + l + 1;
+                    self[l..r].sort_unstable_by_key(|row| row & mask);
+                }
+            }
+            unsorted &= !mask;
+        }
+    }
+
+    fn transpose(&mut self) {
         unsafe fn swap(a: *mut u8, b: *mut u8, j: u32, mask: u8) {
             let t = (*a ^ (*b >> j)) & mask;
             *a ^= t;
@@ -119,110 +222,14 @@ impl BitMatrix8x8 {
     }
 }
 
-impl Debug for BitMatrix8x8 {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "BitMatrix8x8([ ")?;
-        for row in self.rows() {
-            write!(f, "0b{row:08b}, ")?;
-        }
-        write!(f, "])")?;
-        Ok(())
-    }
-}
-
-impl Display for BitMatrix8x8 {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        let mut first = true;
-        write!(f, "[ ")?;
-        for row in self.rows() {
-            if !first {
-                write!(f, "\n  ")?;
-            } else {
-                first = false;
-            }
-            for pos in 0..8 {
-                write!(f, "{} ", (row >> pos) & 1)?;
-            }
-        }
-        write!(f, "]")?;
-        Ok(())
-    }
-}
-
-impl Mul for BitMatrix8x8 {
-    type Output = Self;
-
-    /// Multiplies the matrix by `rhs`.
-    ///
-    /// ```
-    /// # use bitmatrix::BitMatrix8x8;
-    /// let matrix = BitMatrix8x8::new([
-    ///     0b_1_1_1_1_1_1_1_1,
-    ///     0b_0_1_1_1_1_1_1_1,
-    ///     0b_0_0_1_1_1_1_1_1,
-    ///     0b_0_0_0_1_1_1_1_1,
-    ///     0b_0_0_0_0_1_1_1_1,
-    ///     0b_0_0_0_0_0_1_1_1,
-    ///     0b_0_0_0_0_0_0_1_1,
-    ///     0b_0_0_0_0_0_0_0_1,
-    /// ]);
-    /// assert_eq!(matrix * BitMatrix8x8::IDENTITY, matrix);
-    /// ```
-    fn mul(self, rhs: Self) -> Self {
-        const ROW: u64 = 0x00000000000000FF;
-        const COL: u64 = 0x0101010101010101;
-
-        let this = u64::from_ne_bytes(self.0);
-        let rhs = u64::from_ne_bytes(rhs.0);
-        let mut result = 0;
-
-        result |= (COL & this) * (ROW & rhs);
-        result |= (COL & (this >> 1)) * (ROW & (rhs >> 8));
-        result |= (COL & (this >> 2)) * (ROW & (rhs >> 16));
-        result |= (COL & (this >> 3)) * (ROW & (rhs >> 24));
-        result |= (COL & (this >> 4)) * (ROW & (rhs >> 32));
-        result |= (COL & (this >> 5)) * (ROW & (rhs >> 40));
-        result |= (COL & (this >> 6)) * (ROW & (rhs >> 48));
-        result |= (COL & (this >> 7)) * (ROW & (rhs >> 56));
-
-        Self(result.to_ne_bytes())
-    }
-}
-
-impl Mul<&BitMatrix8x8> for BitMatrix8x8 {
-    type Output = <BitMatrix8x8 as Mul>::Output;
-
-    /// Multiplies the matrix by `rhs`. See [BitMatrix8x8::mul] for an example.
-    fn mul(self, rhs: &Self) -> BitMatrix8x8 {
-        self * *rhs
-    }
-}
-
-impl Mul<&BitMatrix8x8> for &BitMatrix8x8 {
-    type Output = <BitMatrix8x8 as Mul>::Output;
-
-    /// Multiplies the matrix by `rhs`. See [BitMatrix8x8::mul] for an example.
-    fn mul(self, rhs: &BitMatrix8x8) -> BitMatrix8x8 {
-        *self * *rhs
-    }
-}
-
-impl Mul<BitMatrix8x8> for &BitMatrix8x8 {
-    type Output = <BitMatrix8x8 as Mul>::Output;
-
-    /// Multiplies the matrix by `rhs`. See [BitMatrix8x8::mul] for an example.
-    fn mul(self, rhs: BitMatrix8x8) -> BitMatrix8x8 {
-        *self * rhs
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn mul_8x8() {
-        let matrix = BitMatrix8x8::new([
+        type BitMatrix8x8 = [u8; 8];
+        let matrix = [
             0b_1_1_1_1_1_1_1_1,
             0b_0_1_1_1_1_1_1_1,
             0b_0_0_1_1_1_1_1_1,
@@ -231,13 +238,14 @@ mod tests {
             0b_0_0_0_0_0_1_1_1,
             0b_0_0_0_0_0_0_1_1,
             0b_0_0_0_0_0_0_0_1,
-        ]);
-        assert_eq!(matrix * BitMatrix8x8::IDENTITY, matrix);
+        ];
+        let identity = BitMatrix8x8::IDENTITY;
+        assert_eq!(matrix.matmul(&identity), matrix);
     }
 
     #[test]
     fn transpose_8x8() {
-        let mut matrix = BitMatrix8x8::new([
+        let mut matrix = [
             0b_1_0_0_0_0_0_0_0,
             0b_1_1_0_0_0_0_0_0,
             0b_1_1_1_0_0_0_0_0,
@@ -246,11 +254,11 @@ mod tests {
             0b_1_1_1_1_1_1_0_0,
             0b_1_1_1_1_1_1_1_0,
             0b_1_1_1_1_1_1_1_1,
-        ]);
+        ];
         matrix.transpose();
         assert_eq!(
             matrix,
-            BitMatrix8x8([
+            [
                 0b_1_1_1_1_1_1_1_1,
                 0b_0_1_1_1_1_1_1_1,
                 0b_0_0_1_1_1_1_1_1,
@@ -259,7 +267,7 @@ mod tests {
                 0b_0_0_0_0_0_1_1_1,
                 0b_0_0_0_0_0_0_1_1,
                 0b_0_0_0_0_0_0_0_1,
-            ])
+            ]
         );
     }
 }
