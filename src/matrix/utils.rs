e49a012d7b8f2b7404bf65de75e7e151688c02e3
j.v.o.koskinen@gmail.com <j.v.o.koskinen@gmail.com>
2024-10-19 13:28:45 +0300

    add utility functions for the four russians method

--- src/matrix/utils.rs
+++ src/matrix/utils.rs
@@ -13,6 +13,11 @@ pub(crate) fn delta_swap(x: u64, m: u64, shift: u32) -> u64 {
     (x ^ t) ^ (t << shift)
 }
 
+/// Returns `a` divided by `b` rounded up.
+pub(crate) const fn div_ceil(a: usize, b: usize) -> usize {
+    (a + b - 1) / b
+}
+
 /// Exchanges the masked bits of `x` with the corresponding bits in `y`, and returns the
 /// result.
 ///
@@ -31,6 +36,11 @@ pub(crate) fn exchange(mut x: u64, mut y: u64, mask: u64) -> (u64, u64) {
     (x, y)
 }
 
+/// Computes the gray code of a number `i`.
+const fn gray_code(i: usize) -> usize {
+    i ^ (i >> 1)
+}
+
 /// Computes the integer square root of `n` using Newton's method.
 const fn isqrt(n: usize) -> usize {
     let mut x = n;
@@ -40,6 +50,35 @@ const fn isqrt(n: usize) -> usize {
         y = (x + n / x) / 2;
     }
     x
+}
+
+/// Computes an index into the row sum table in the four russians algorithm. The element at `prefix`
+/// is the index of the row sum for that prefix.
+pub(crate) const fn row_sum_index<const N: usize>() -> [usize; N] {
+    let mut codes = [0; N];
+    let mut i = 1;
+    while i < N {
+        codes[gray_code(i)] = i;
+        i += 1;
+    }
+    codes
+}
+
+/// Computes an index into the stripe for constructing the row sum table in the four russians
+/// algorithm. The element at `index` is the index of the row that should be `XOR`ed with the
+/// previous row sum.
+pub(crate) const fn stripe_index<const N: usize>() -> [usize; N] {
+    let mut diffs = [0; N];
+    let mut prev = 0;
+    let mut i = 1;
+    while i < N {
+        let code = gray_code(i);
+        diffs[i] = (code ^ prev).ilog2() as usize;
+        // diffs[i] = (usize::BITS - (code ^ prev).leading_zeros() - 1) as usize;
+        prev = code;
+        i += 1
+    }
+    diffs
 }
 
 /// Constructs a mask for the `transpose` operation. The mask can be thought of as a bit matrix
@@ -61,13 +100,13 @@ pub(crate) const fn transpose_mask<const N: usize>(block: usize) -> [u64; N] {
 
 /// Moves the masked bits in `x` to the left by `shift` positions. This is a generalization of
 /// `delta_swap` to `u64` arrays. See `delta_swap` for more information.
-pub(crate) fn w_delta_swap<const N: usize>(x: [u64; N], m: [u64; N], shift: u32) -> [u64; N] {
-    let t = w_and(w_xor(w_shr(x, shift), x), m);
-    w_xor(w_xor(x, t), w_shl(t, shift))
+pub(crate) fn wide_delta_swap<const N: usize>(x: [u64; N], m: [u64; N], shift: u32) -> [u64; N] {
+    let t = wide_and(wide_xor(wide_shr(x, shift), x), m);
+    wide_xor(wide_xor(x, t), wide_shl(t, shift))
 }
 
 /// Performs the `and` operation on two unsigned integers stored in `u64` arrays.
-pub(crate) fn w_and<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
+pub(crate) fn wide_and<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
     let mut result = [0; N];
     for i in 0..N {
         result[i] = a[i] & b[i];
@@ -76,7 +115,7 @@ pub(crate) fn w_and<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
 }
 
 /// Performs the `xor` operation on two unsigned integers stored in `u64` arrays
-pub(crate) fn w_xor<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
+pub(crate) fn wide_xor<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
     let mut result = [0; N];
     for i in 0..N {
         result[i] = a[i] ^ b[i];
@@ -87,7 +126,7 @@ pub(crate) fn w_xor<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
 /// Multiplies two unsigned integers using the grade-school method. The arguments and
 /// the result are arrays of `u64`s, where the least significant word is at index 0.
 /// The result is truncated if the multiplication overflows.
-pub(crate) fn w_mul<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
+pub(crate) fn wide_mul<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
     let mut result = [0; N];
     for i in 0..N {
         let mut carry: u64 = 0;
@@ -100,21 +139,8 @@ pub(crate) fn w_mul<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
     result
 }
 
-/// An optimized version of `w_mul` for the case where the `b` array has only one non-zero element
-/// at index 0.
-pub(crate) fn w_mul_n1<const N: usize>(a: [u64; N], b: [u64; N]) -> [u64; N] {
-    let mut result = [0; N];
-    let mut carry: u64 = 0;
-    for i in 0..N {
-        let t = a[i] as u128 * b[0] as u128 + result[i] as u128 + carry as u128;
-        result[i] = t as u64;
-        carry = (t >> 64) as u64;
-    }
-    result
-}
-
 /// Performs the right shift operation. The least significant word is assumed to be at index 0.
-pub(crate) fn w_shr<const N: usize>(mut v: [u64; N], shift: u32) -> [u64; N] {
+pub(crate) fn wide_shr<const N: usize>(mut v: [u64; N], shift: u32) -> [u64; N] {
     let delta = shift as usize / 64;
     let shift = shift % 64;
     let mask = !(!0 >> shift);
@@ -131,7 +157,7 @@ pub(crate) fn w_shr<const N: usize>(mut v: [u64; N], shift: u32) -> [u64; N] {
 }
 
 /// Performs the left shift operation. The least significant word is assumed to be at index 0.
-pub(crate) fn w_shl<const N: usize>(mut v: [u64; N], shift: u32) -> [u64; N] {
+pub(crate) fn wide_shl<const N: usize>(mut v: [u64; N], shift: u32) -> [u64; N] {
     let delta = shift as usize / 64;
     let shift = shift % 64;
     let mask = !(!0 << shift);
