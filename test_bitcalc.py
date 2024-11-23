ab5c6ee09ad3664e9f6321674adfb26ffdf19b1b
j.v.o.koskinen@gmail.com <j.v.o.koskinen@gmail.com>
2024-11-23 10:45:56 +0200

    add randomized test for expressions and refactor other tests

--- test_bitcalc.py
+++ test_bitcalc.py
@@ -10,32 +10,149 @@ def test_eq():
     assert a != b
 
     # Double negation
-    assert a == Not(Not(a))
+    assert a == ~(~a)
 
     # Commutativity
-    assert And(a, b) == And(b, a)
-    assert Or(a, b) == Or(b, a)
-    assert Xor(a, b) == Xor(b, a)
+    assert a & b == b & a
+    assert a | b == b | a
+    assert a ^ b == b ^ a
 
     # Associativity
-    assert And(a, And(b, c)) == And(And(a, b), c)
-    assert Or(a, Or(b, c)) == Or(Or(a, b), c)
-    assert Xor(a, Xor(b, c)) == Xor(Xor(a, b), c)
+    assert a & (b & c) == (a & b) & c == a & b & c
+    assert a | (b | c) == (a | b) | c == a | b | c
+    assert a ^ (b ^ c) == (a ^ b) ^ c == a ^ b ^ c
 
     # Distributivity
-    assert And(a, Or(b, c)) == Or(And(a, b), And(a, c))
-    assert Or(a, And(b, c)) == And(Or(a, b), Or(a, c))
+    assert a & (b | c) == (a & b) | (a & c)
+    assert a | (b & c) == (a | b) & (a | c)
 
     # Idempotence
-    assert And(a, a) == a
-    assert Or(a, a) == a
-    assert Xor(a, a) == Bit("0")
+    assert a & a == a
+    assert a | a == a
+    assert a ^ a == Bit(0)
 
     # Absorption
-    assert Or(a, And(a, b)) == a
+    assert a & (a | b) == a
 
     # De Morgan's laws
-    assert Not(And(a, b)) == Or(Not(a), Not(b))
+    assert ~(a & b) == ~a | ~b
+
+
+def test_randomized():
+    from random import randrange
+
+    BITS = 3
+    MIN_COUNT = 10
+    tt_exprs = {}
+
+    def rand_bit() -> Bit:
+        return Bit(chr(ord("a") + randrange(BITS)))
+
+    def rand_op() -> "Expr":
+        match randrange(3):
+            case 0:
+                return And
+            case 1:
+                return Or
+            case 2:
+                return Xor
+
+    # Generate at least MIN_COUNT truth tables with at least two expressions each.
+    while sum(1 for e in tt_exprs.values() if len(e) > 1) < MIN_COUNT:
+        expr = rand_bit()
+        while len(expr.vars()) < BITS:
+            rhs = rand_op()(rand_bit(), rand_bit())
+            expr = rand_op()(expr, rhs).simplify()
+
+        tt = str(expr.truth_table())
+
+        # If the expression is already in the dictionary, skip it.
+        if repr(expr) not in [repr(e) for e in tt_exprs.get(tt, [])]:
+            tt_exprs[tt] = tt_exprs.get(tt, []) + [expr]
+
+    # Check that all expressions with the same truth table are equal.
+    for tt, exprs in tt_exprs.items():
+        for i, expr_a in enumerate(exprs):
+            for expr_b in exprs[:i]:
+                assert expr_a == expr_b
+
+    # Keep the first expression for each truth table.
+    exprs = [exprs[0] for exprs in tt_exprs.values()]
+
+    # Check that no two expressions in the list are equal.
+    for a in exprs:
+        for b in exprs:
+            if a == b:
+                assert repr(a) == repr(b)
+
+
+def test_simplify():
+    a = Bit("a")
+
+    def test(expr: "Expr", expect_repr: str):
+        assert str(expr.simplify()) == expect_repr
+
+    # Simplify a AND a
+    test(a & a, "a")
+
+    # Simplify a OR a
+    test(a | a, "a")
+
+    # Simplify a AND NOT a
+    test(a & ~a, "0")
+
+    # Simplify a OR NOT a
+    test(a | ~a, "1")
+
+    # Simplify a AND 1
+    test(a & Bit(1), "a")
+
+    # Simplify a OR 0
+    test(a | Bit(0), "a")
+
+    # Simplify a AND 0
+    test(a & Bit(0), "0")
+
+    # Simplify a OR 1
+    test(a | Bit(1), "1")
+
+    # Simplify a XOR a
+    test(a ^ a, "0")
+
+    # Simplify a XOR 0
+    test(a ^ Bit(0), "a")
+
+
+def test_str():
+    a = Bit("a")
+    b = Bit("b")
+    c = Bit("c")
+
+    assert str(a) == "a"
+    assert str(Not(a)) == "~a"
+    assert str(And(a, b)) == "(a & b)"
+    assert str(Or(b, a)) == "(a | b)"
+    assert str(Xor(a, b)) == "(a ^ b)"
+    assert str(And(Or(a, b), c)) == "((a | b) & c)"
+    assert str(Or(c, And(a, b))) == "((a & b) | c)"
+    assert str(Xor(And(a, b), c)) == "((a & b) ^ c)"
+
+
+def test_to_int():
+    assert Bit(0).to_int() == 0
+    assert Bit(1).to_int() == 1
+    assert Bit("a").to_int() is None
+
+
+def test_truth_table():
+    a = Bit("a")
+    b = Bit("b")
+
+    assert a.truth_table() == [1]
+    assert Not(a).truth_table() == [0]
+    assert And(a, b).truth_table() == [3]
+    assert Or(a, b).truth_table() == [1, 2, 3]
+    assert Xor(a, b).truth_table() == [1, 2]
 
 
 def test_uint_simple():
@@ -47,6 +164,7 @@ def test_uint_simple():
     assert int(one) == 1
     assert int(two) == 2
     assert one + one == two
+    assert two - one == one
     assert two * one == two
 
     # Overflow:
@@ -96,9 +214,8 @@ def test_uint_randomized():
     def trunc(x):
         return MASK & x
 
-    for i in range(COUNT):
-        a = randrange(MAX)
-        b = randrange(MAX)
+    for _ in range(COUNT):
+        [a, b] = sorted([randrange(MAX), randrange(MAX)])
 
         uint_a = UInt(a, WIDTH)
         uint_b = UInt(b, WIDTH)
@@ -111,45 +228,9 @@ def test_uint_randomized():
         assert trunc(rotr(a, 2)) == int(uint_a.rotate_right(2))
 
         assert trunc(a + b) == int(uint_a + uint_b)
+        assert trunc(b - a) == int(uint_b - uint_a)
         assert trunc(a * b) == int(uint_a * uint_b)
         assert trunc(a & b) == int(uint_a & uint_b)
         assert trunc(a | b) == int(uint_a | uint_b)
         assert trunc(a ^ b) == int(uint_a ^ uint_b)
         assert trunc(~a) == int(~uint_a)
-
-
-def test_simplify():
-    a = Bit("a")
-
-    def test(expr: "Expr", expect_repr: str):
-        assert str(expr.simplify()) == expect_repr
-
-    # Simplify a AND a
-    test(And(a, a), "a")
-
-    # Simplify a OR a
-    test(Or(a, a), "a")
-
-    # Simplify a AND NOT a
-    test(And(a, Not(a)), "0")
-
-    # Simplify a OR NOT a
-    test(Or(a, Not(a)), "1")
-
-    # Simplify a AND 1
-    test(And(a, Bit("1")), "a")
-
-    # Simplify a OR 0
-    test(Or(a, Bit("0")), "a")
-
-    # Simplify a AND 0
-    test(And(a, Bit("0")), "0")
-
-    # Simplify a OR 1
-    test(Or(a, Bit("1")), "1")
-
-    # Simplify a XOR a
-    test(Xor(a, a), "0")
-
-    # Simplify a XOR 0
-    test(Xor(a, Bit("0")), "a")
